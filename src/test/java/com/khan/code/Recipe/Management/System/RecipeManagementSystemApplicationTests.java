49a033dc081aa9769f4459e804ae99a2cf2616c8
abdulwariskhan <hellokhan.ch@gmail.com>
2025-05-05 05:45:15 +0200

    Initial commit

---
+++ src/test/java/com/khan/code/Recipe/Management/System/RecipeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khan.code.Recipe.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
